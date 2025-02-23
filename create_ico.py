0b9c285c1eb974e45467c5cc4f2d02eb3dec56c6
manuhume <manu@clutchlabs.com.au>
2025-02-23 17:33:34 +1100

    minor icon changes did not work

--- create_ico.py
+++ create_ico.py
@@ -2,23 +2,31 @@ from PIL import Image
 import os
 
 def create_ico():
-    # Define the sizes we want in our ico file
-    sizes = [16, 32, 48, 64, 128, 256]
+    # Standard Windows icon sizes
+    sizes = [16, 24, 32, 48, 64, 128, 256]
     
-    # Load the largest image as our source
+    # Load the highest resolution image as our source
     img = Image.open('assets/icons.iconset/icon_512x512@2x.png')
     
     # Create a list to store our images
     imgs = []
     
-    # Resize the image to all the sizes we need
+    # Process each size
     for size in sizes:
         resized_img = img.resize((size, size), Image.Resampling.LANCZOS)
+        if resized_img.mode != 'RGBA':
+            resized_img = resized_img.convert('RGBA')
         imgs.append(resized_img)
     
-    # Save all sizes to a single .ico file
-    # The first image in the list will be the default size
-    imgs[0].save('assets/AppIcon.ico', format='ICO', sizes=[(img.width, img.height) for img in imgs], append_images=imgs[1:])
+    # Save as ICO with all sizes
+    # Windows typically uses 32x32 as the default
+    idx_32 = sizes.index(32)
+    imgs.insert(0, imgs.pop(idx_32))
+    
+    # Save with all sizes included
+    imgs[0].save('assets/AppIcon.ico', 
+                format='ICO',
+                sizes=[(size, size) for size in sizes])
 
 if __name__ == '__main__':
     create_ico() 
